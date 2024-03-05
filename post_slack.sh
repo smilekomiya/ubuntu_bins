82fec3456dba0705a4439c50a65a405e4b17f196
smilekomiya <yuzuru.komiya@gmail.com>
2024-03-06 01:50:44 +0900

    post slack message

--- post_slack.sh
+++ post_slack.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-webhook_url=$POST_SLACK_WEBHOOK_URL
-if [ -z "$webhook_url" ]; then
-  echo "environment variable POST_SLACK_WEBHOOK_UR is not set"
-  eixt 1
-fi
 
+post() {
+  local  webhook_url=$POST_SLACK_WEBHOOK_URL
+  if [ -z "$webhook_url" ]; then
+    echo "environment variable POST_SLACK_WEBHOOK_URL is not set"
+    exit 1
+  fi
+
+  local message=$(cat -)
+  curl -s -X POST \
+    -H 'Content-type: application/json' \
+    --data "{'text': \"$message\"}" \
+    $webhook_url > /dev/null 
+}
+
+
+if [ -p /dev/stdin ]; then
+  cat -
+else
+  echo $1
+fi | post
  
+
